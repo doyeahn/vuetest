f4f4c0c521ef6724fe5cbae9027f515c8c68b0af
doyeahn <doyeahn@naver.com>
2020-03-17 16:06:45 +0900

    test commit

---
+++ mm/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+   <div  id="App">
+      <div>  
+        <nav>
+          <ul>
+            <li>
+              <router-link v-bind:to="{name:'home'}">홈</router-link></li>
+            <li @click="time" v-bind:to="{name:'time'}">시간</li>
+            <li><router-link v-bind:to="{name: 'search'}">검색</router-link></li>
+        </ul>
+        </nav>
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
+    </div>
+</template>
+
+<script>
+
+//import Home from './components/Home.vue'
+//import Time from './components/Time.vue'
+import Search from './components/Search.vue'
+import VueRouter from 'vue-router'
+//import Login from './components/Login.vue'
+
+export default {
+  name: 'App', router,
+  components: {
+ Search//, Time, Login
+
+ }
+
+}
+
+const router = new VueRouter({
+  routes : [
+   // {path: '/', component: Home},
+  //  {path:'/home', name:'home', component: Home},
+   // {path:'/time', name:'time', component: Time},
+    {path: '/search', name:'search', component: Search} //,
+    //{path: '/login', name:'login', component: Login}
+    //beforeRouteEnter: (to, from, next)=>{}
+  ]
+  
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.nav {float: left; width: 200px;}
+
+</style>
---
+++ mm/src/components/Search.vue
@@ -0,0 +1,44 @@
+ <style>
+.score {
+  border: solid 1px black;
+}
+.warning {
+  background-color: orange;
+  color: purple;
+}
+.warnimage {
+  width: 18px;
+  height: 18px;
+  top: 5px;
+  position: relative;
+}
+</style>
+
+<template>
+  <div id="search">
+    <div>
+      입력:
+      <input type="text" class="score" v-model="player" />
+      <img src="assets/error.jpg" class="warnimage" v-show="info.warning" />
+    </div>
+  </div>
+</template>
+    <!--    <script src="https://unpkg.com/vue@2.5.16/dist/vue.js"></script>
+ --> 
+
+<script>
+export default {
+  name: "search",
+  data: function(){
+      return {player:''}
+  },
+  computed:
+{
+    info: function(){
+            if(this.player === 'seungwoolee')
+            return { waring : true};
+            else return {warning : false};
+}
+}
+}
+</script>
