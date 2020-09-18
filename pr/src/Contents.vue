633ccfc8f475f113823a59b8e5a8f7eb1b8a76f6
doyeahn <doyeahn@naver.com>
2020-09-18 18:09:19 +0900

    itc commit

---
+++ pr/src/Contents.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>abc</h1>
+        <h3>{{(new Date()).toTimeString()}}
+        </h3>
+        <h4> 조직도</h4>
+        <tree :subs="contents" ></tree>
+    </div>
+
+
+</template>
+
+<script type="text/javascript">
+import tree from './Tree.vue';
+
+export default{
+    name:'contents',
+    components:{tree},
+    data: function()
+    {
+        return {
+            contents:[
+                {
+                    name:"RMCF", type:"company",
+                    subs: [
+                        {name: "SI 사업부", type:"division",
+                            subs: [
+                                {name: "SI 1팀", type: "team"},
+                                {name:"SI 2팀", type:"team"}
+                            ]},
+                    {name:"BI 사업부", type: "division",
+                    subs : [
+                        {name: "bi-1", type:"team"},
+                        {name: "bi-2", type:"team"}
+                    ]}
+                    ]
+                }
+            ]
+        }
+    }
+}
+</script>
---
+++ pr/src/Tree.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li v-for="s in subs" :class="s.type" :key="s.name">
+            {{s.name}}
+            <tree :subs="s.subs"></tree>
+        </li>
+    </ul>
+
+
+</template>
+<script type="text/javascript">
+export default{
+    name:'tree',
+    props:['subs']
+}
+</script>
